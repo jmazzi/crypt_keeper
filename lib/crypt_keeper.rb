8abb1df062183f28f62b0d308a86a22c4a8f2e4a
Justin Mazzi <jmazzi@gmail.com>
2012-10-04 22:33:34 -0400

    Move common SQL methods to a helper

--- lib/crypt_keeper.rb
+++ lib/crypt_keeper.rb
@@ -2,7 +2,7 @@ require 'active_record'
 
 require 'crypt_keeper/version'
 require 'crypt_keeper/model'
-
+require 'crypt_keeper/helper'
 require 'crypt_keeper/provider/aes'
 require 'crypt_keeper/provider/mysql_aes'
 require 'crypt_keeper/provider/postgres_pgp'
