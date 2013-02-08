569f3d36f7a05d2f5dbc888df15c1fab29f6688d
Frank C. Eckert <frank.eckert@boost-project.com>
2013-02-08 18:54:00 +0100

    trying to bring back 1.9.2

--- spec/support/mongoid.rb
+++ spec/support/mongoid.rb
@@ -1,29 +1,31 @@
-require 'mongoid'
-require 'logger'
+unless RUBY_VERSION == '1.9.2'
+  require 'mongoid'
+  require 'logger'
 
-::Mongoid.logger = Logger.new SPEC_ROOT.join('debug.log').to_s
+  ::Mongoid.logger = Logger.new SPEC_ROOT.join('debug.log').to_s
 
-module CryptKeeper
-  class SensitiveMongo
-    include Mongoid::Document
-    include CryptKeeper::Model
+  module CryptKeeper
+    class SensitiveMongo
+      include Mongoid::Document
+      include CryptKeeper::Model
 
-    field :storage, type: String
-    field :secret, type: String
-    field :name, type: Integer
-  end
+      field :storage, type: String
+      field :secret, type: String
+      field :name, type: Integer
+    end
 
-  module MongoidConnectionHelper
-    def use_mongoid
-      before :all do
-        config = YAML.load_file SPEC_ROOT.join('database.yml')
-        ::Mongoid::Config.load_configuration(config['mongoid'])
+    module MongoidConnectionHelper
+      def use_mongoid
+        before :all do
+          config = YAML.load_file SPEC_ROOT.join('database.yml')
+          ::Mongoid::Config.load_configuration(config['mongoid'])
+        end
       end
     end
   end
-end
 
 
-RSpec.configure do |config|
-  config.extend CryptKeeper::MongoidConnectionHelper
+  RSpec.configure do |config|
+    config.extend CryptKeeper::MongoidConnectionHelper
+  end
 end
