2768cd8894d522e2dec2f5081f757941952af9f1
Justin Mazzi <jmazzi@gmail.com>
2013-12-05 02:31:21 +0000

    Add sym decrypt class

--- lib/crypt_keeper/provider/postgres_pgp.rb
+++ lib/crypt_keeper/provider/postgres_pgp.rb
@@ -2,11 +2,31 @@ require 'crypt_keeper/log_subscriber/postgres_pgp'
 
 module CryptKeeper
   module Provider
-    class PostgresPgp
+    class PostgresPgpSym
       include CryptKeeper::Helper::SQL
 
+      def initialize(key, options)
+        @key     = key
+        @options = options
+      end
+
+      # Public: Encrypts a string
+      #
+      # Returns an encrypted string
+      def encrypt(value)
+        escape_and_execute_sql(["SELECT pgp_sym_encrypt(?, ?, ?)", value.to_s, @key, @options])['pgp_sym_encrypt']
+      end
+
+      # Public: Decrypts a string
+      #
+      # Returns a plaintext string
+      def decrypt(value)
+        escape_and_execute_sql(["SELECT pgp_sym_decrypt(?, ?)", value, @key])['pgp_sym_decrypt']
+      end
+    end
+
+    class PostgresPgp
       attr_accessor :key
-      attr_accessor :pgcrypto_options
 
       # Public: Initializes the encryptor
       #
@@ -18,25 +38,30 @@ module CryptKeeper
           raise ArgumentError, "Missing :key"
         end
 
-        @pgcrypto_options = options.fetch(:pgcrypto_options, '')
+        @pgcrypto_options = options.delete(:pgcrypto_options) || ''
+        @options          = options
       end
 
       # Public: Encrypts a string
       #
       # Returns an encrypted string
       def encrypt(value)
-        escape_and_execute_sql(["SELECT pgp_sym_encrypt(?, ?, ?)", value.to_s, key, pgcrypto_options])['pgp_sym_encrypt']
+        decrypt_class.new(key, @pgcrypto_options).encrypt(value)
       end
 
       # Public: Decrypts a string
       #
       # Returns a plaintext string
       def decrypt(value)
-        escape_and_execute_sql(["SELECT pgp_sym_decrypt(?, ?)", value, key])['pgp_sym_decrypt']
+        decrypt_class.new(key, @pgcrypto_options).decrypt(value)
       end
 
       def search(records, field, criteria)
         records.where("(pgp_sym_decrypt(cast(#{field} AS bytea), ?) = ?)", key, criteria)
+      end
+
+      def decrypt_class
+        @options[:public] ? PostgresPgpPublic : PostgresPgpSym
       end
     end
   end
